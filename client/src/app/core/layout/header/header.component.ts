57e0d3a3039143b1f0475d08cc89b2c5f667d9fb
Ta7ar <imontatar@gmail.com>
2026-02-21 21:54:52 -0500

    pre built dist folder

--- client/src/app/core/layout/header/header.component.ts
+++ client/src/app/core/layout/header/header.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, inject, output } from '@angular/core';
-import { RouterLink, RouterLinkActive } from "@angular/router";
+import { RouterLink } from "@angular/router";
 import { MatToolbar } from "@angular/material/toolbar"
 import { MatButton } from '@angular/material/button';
 import { NgOptimizedImage } from '@angular/common';
@@ -9,7 +9,7 @@ import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-header',
-    imports: [RouterLink, RouterLinkActive, MatToolbar, MatButton, NgOptimizedImage, MatBadgeModule, MatIconModule],
+    imports: [RouterLink, MatToolbar, MatButton, NgOptimizedImage, MatBadgeModule, MatIconModule],
     templateUrl: './header.component.html',
     styleUrl: './header.component.css',
     standalone: true,
--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MatButton } from '@angular/material/button';
 import {FlexLayoutModule} from "@angular/flex-layout";
 import { RedactionComponent } from '../../core/components/redaction/redaction.component';
 import { MatListModule } from '@angular/material/list';
@@ -7,7 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-home',
-    imports: [MatButton, FlexLayoutModule, RedactionComponent, MatListModule, MatIconModule],
+    imports: [FlexLayoutModule, RedactionComponent, MatListModule, MatIconModule],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
     standalone: true,
